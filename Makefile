61c00250079d914d4dc839fecd694f68c02ddaf2
sirawatK <sirawatm123@gmail.com>
2023-10-14 00:20:22 +0700

    use va_list in para

--- Makefile
+++ Makefile
@@ -3,18 +3,17 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: sikulnok <sikulnok@student.42.fr>          +#+  +:+       +#+         #
+#    By: sikulnok <sikulnok@student.42bangkok.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/10/13 16:26:39 by sikulnok          #+#    #+#              #
-#    Updated: 2023/10/13 22:18:46 by sikulnok         ###   ########.fr        #
+#    Updated: 2023/10/14 00:17:01 by sikulnok         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = libftprintf.a
 
 SRCS = srcs/*.c
-
-INCLUDE = include/ft_printf.h
+INCLUDE = include/
 
 OBJS = *.o
 
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -3,34 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sikulnok <sikulnok@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sikulnok <sikulnok@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 16:36:20 by sikulnok          #+#    #+#             */
-/*   Updated: 2023/10/13 23:52:58 by sikulnok         ###   ########.fr       */
+/*   Updated: 2023/10/14 00:10:45 by sikulnok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-int	print_format(char specifier, va_list *ap)
+int	print_format(char specifier, va_list ap)
 {
 	int	printed;
 
 	printed = 0;
 	if (specifier == 'c')
-		printed = print_char(va_arg(*ap, int));
+		printed = print_char(va_arg(ap, int));
 	else if (specifier == 's')
-		printed = print_str(va_arg(*ap, char *));
+		printed = print_str(va_arg(ap, char *));
 	else if (specifier == 'd' || specifier == 'i')
-		printed = print_decimal(va_arg(*ap, int));
+		printed = print_decimal(va_arg(ap, int));
 	else if (specifier == 'u')
-		printed = print_unsigned(va_arg(*ap, unsigned int));
+		printed = print_unsigned(va_arg(ap, unsigned int));
 	else if (specifier == 'x')
-		printed = print_hex(va_arg(*ap, unsigned int), 0);
+		printed = print_hex(va_arg(ap, unsigned int), 0);
 	else if (specifier == 'X')
-		printed = print_hex(va_arg(*ap, unsigned int), 1);
+		printed = print_hex(va_arg(ap, unsigned int), 1);
 	else if (specifier == 'p')
-		printed = print_ptr(va_arg(*ap, unsigned long long int));
+		printed = print_ptr(va_arg(ap, unsigned long long int));
 	else if (specifier == '%')
 		printed = write(1, "%", 1);
 	return (printed);
@@ -47,7 +47,7 @@ int	ft_printf(const char *format, ...)
 	while (*format)
 	{
 		if (*format == '%')
-			error = print_format(*(++format), &ap);
+			error = print_format(*(++format), ap);
 		else
 			error = print_char(*format);
 		if (error == -1)
